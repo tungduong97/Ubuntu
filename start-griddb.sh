e312582a024c31bf2c15a41a6df4ed12bd13ca69
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-08-21 02:02:50 +0700

    Update start-griddb.sh

--- start-griddb.sh
+++ start-griddb.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-GRIDDB_CLUSTER_NAME=$(sed -nr "s/(GRIDDB_CLUSTER_NAME)=*//p" config.txt)
-GRIDDB_USERNAME=$(sed -nr "s/(GRIDDB_USERNAME)=*//p" config.txt)
-GRIDDB_PASSWORD=$(sed -nr "s/(GRIDDB_PASSWORD)=*//p" config.txt)
 
-su - gsadm -c "gs_passwd $GRIDDB_USERNAME -p $GRIDDB_PASSWORD"
+if [ -z "$GRIDDB_CLUSTER_NAME" ]
+then
+    GRIDDB_CLUSTER_NAME=$(sed -nr "s/(GRIDDB_CLUSTER_NAME)=*//p" config.txt)
+    GRIDDB_USERNAME=$(sed -nr "s/(GRIDDB_USERNAME)=*//p" config.txt)
+    GRIDDB_PASSWORD=$(sed -nr "s/(GRIDDB_PASSWORD)=*//p" config.txt)
 
-sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"$GRIDDB_CLUSTER_NAME\"/g \/var/lib/gridstore/conf/gs_cluster.json
+    su - gsadm -c "gs_passwd $GRIDDB_USERNAME -p $GRIDDB_PASSWORD"
 
-su -c "gs_startnode; gs_joincluster -c $GRIDDB_CLUSTER_NAME -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD" - gsadm
+    sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"$GRIDDB_CLUSTER_NAME\"/g \/var/lib/gridstore/conf/gs_cluster.json
 
-tail -f /dev/null
+    su -c "gs_startnode; gs_joincluster -c $GRIDDB_CLUSTER_NAME -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD" - gsadm
+
+    tail -f /dev/null
+else
+    su - gsadm -c "gs_passwd $GRIDDB_USERNAME -p $GRIDDB_PASSWORD"
+
+    sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"$GRIDDB_CLUSTER_NAME\"/g \/var/lib/gridstore/conf/gs_cluster.json
+
+    su -c "gs_startnode; gs_joincluster -c $GRIDDB_CLUSTER_NAME -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD" - gsadm
+
+    tail -f /dev/null
+fi
