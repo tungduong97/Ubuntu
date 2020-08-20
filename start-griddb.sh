86eaf149db2571a8ad69a9ecf8c6cbff1596de7c
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-08-21 00:50:07 +0700

    Update start-griddb.sh

--- start-griddb.sh
+++ start-griddb.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-su -c "gs_startnode; gs_joincluster -c dockerGridDB -u admin/admin; tail -f /dev/null" - gsadm
+GRIDDB_CLUSTER_NAME=$(sed -nr "s/(GRIDDB_CLUSTER_NAME)=*//p" config.txt)
+GRIDDB_USERNAME=$(sed -nr "s/(GRIDDB_USERNAME)=*//p" config.txt)
+GRIDDB_PASSWORD=$(sed -nr "s/(GRIDDB_PASSWORD)=*//p" config.txt)
+
+su - gsadm -c "gs_passwd $GRIDDB_USERNAME -p $GRIDDB_PASSWORD"
+
+sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"$GRIDDB_CLUSTER_NAME\"/g \/var/lib/gridstore/conf/gs_cluster.json
+
+su -c "gs_startnode; gs_joincluster -c $GRIDDB_CLUSTER_NAME -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD" - gsadm
+
+tail -f /dev/null
