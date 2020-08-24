fbc010a6c8f7b524be8c32f388daa76e219a8474
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-08-25 01:22:05 +0700

    Update start-griddb.sh

--- start-griddb.sh
+++ start-griddb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -z "$CONFIG" ]
+var="$1"
+if [ "${1}" = 'griddb' ]
 then
 	GRIDDB_CLUSTER_NAME=$(sed -n 's|.*"clusterName":"\([^"]*\)".*|\1|p' /var/lib/gridstore/conf/gs_cluster.json)
 	if [ -z "$GRIDDB_CLUSTER_NAME" ]
@@ -22,7 +23,6 @@ else
 		sed -i "s/admin/${GRIDDB_USERNAME}/g" /var/lib/gridstore/conf/password
 		su - gsadm -c "gs_passwd $GRIDDB_USERNAME -p $GRIDDB_PASSWORD"
 		sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"$GRIDDB_CLUSTER_NAME\"/g \/var/lib/gridstore/conf/gs_cluster.json
-		sed -i -e s/\"replicationNum\":2/\"replicationNum\":$GRIDDB_NODE_NUM/g \/var/lib/gridstore/conf/gs_cluster.json
 		sed -i -e s/\"notificationAddress\":\"239.0.0.1\"/\"notificationAddress\":\"$NOTIFICATION_ADDRESS\"/g \/var/lib/gridstore/conf/gs_cluster.json
 		sed -i -e s/\"notificationPort\":31999/\"notificationPort\":$NOTIFICATION_PORT/g \/var/lib/gridstore/conf/gs_cluster.json
 		su -c "gs_startnode; gs_joincluster -c $GRIDDB_CLUSTER_NAME -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD" - gsadm
