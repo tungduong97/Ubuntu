bb784bc910770e38a33d6af15bd8c864327abe11
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-08-28 13:59:26 +0700

    Update start-griddb.sh

--- start-griddb.sh
+++ start-griddb.sh
@@ -36,21 +36,23 @@ else
         if [ ! -z "$NOTIFICATION_ADDRESS" ]
         then
             #Config multicast for griddb sever and start griddb sever with single node
-            gs_passwd admin -p admin
-            sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"dockergriddb\"/g \/var/lib/gridstore/conf/gs_cluster.json
-            sed -i -e s/\"notificationAddress\":\"239.0.0.1\"/\"notificationAddress\":\"239.0.0.1\"/g \/var/lib/gridstore/conf/gs_cluster.json
-            sed -i -e s/\"notificationPort\":31999/\"notificationPort\":31999/g \/var/lib/gridstore/conf/gs_cluster.json
+            sed -i "s/admin/${GRIDDB_USERNAME}/g" /var/lib/gridstore/conf/password
+            gs_passwd $GRIDDB_USERNAME -p $GRIDDB_PASSWORD
+            sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"$GRIDDB_CLUSTER_NAME\"/g \/var/lib/gridstore/conf/gs_cluster.json
+            sed -i -e s/\"notificationAddress\":\"239.0.0.1\"/\"notificationAddress\":\"$NOTIFICATION_ADDRESS\"/g \/var/lib/gridstore/conf/gs_cluster.json
+            sed -i -e s/\"notificationPort\":31999/\"notificationPort\":$NOTIFICATION_PORT/g \/var/lib/gridstore/conf/gs_cluster.json
             #Restart griddb sever
-            gs_startnode -u admin/admin
-            gs_joincluster -c dockergriddb -u admin/admin
+            gs_startnode -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
+            gs_joincluster -c $GRIDDB_CLUSTER_NAME -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
             tail -f /var/lib/gridstore/log/gridstore*.log
         else
             #Config setting for griddb sever and start griddb multi node
-            gs_passwd admin -p admin
-            sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"dockergriddb\"/g \/var/lib/gridstore/conf/gs_cluster.json
+            sed -i "s/admin/${GRIDDB_USERNAME}/g" /var/lib/gridstore/conf/password
+            gs_passwd $GRIDDB_USERNAME -p $GRIDDB_PASSWORD
+            sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"$GRIDDB_CLUSTER_NAME\"/g \/var/lib/gridstore/conf/gs_cluster.json
             #Restart griddb sever
-            gs_startnode -u admin/admin
-            gs_joincluster -s 127.20.0.2:10040 -c dockergriddb -n 1 -u admin/admin
+            gs_startnode -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
+            gs_joincluster -s $IP_GRIDDB_NODE:10040 -c $GRIDDB_CLUSTER_NAME -n $GRIDDB_NODE_NUM -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
             tail -f /var/lib/gridstore/log/gridstore*.log
         fi
 
@@ -59,12 +61,12 @@ else
         if [ ! -z "$NOTIFICATION_ADDRESS" ]
         then
             #Start griddb with single node
-            gs_startnode -u admin/admin
-            gs_joincluster -c dockergriddb -u admin/admin
+            gs_startnode -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
+            gs_joincluster -c $GRIDDB_CLUSTER_NAMES -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
         else
             #Start griddb with multi node
-            gs_startnode -u admin/admin
-            gs_joincluster -s 127.20.0.2:10040 -c dockergriddb -n 1 -u admin/admin
+            gs_startnode -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
+            gs_joincluster -s $IP_GRIDDB_NODE:10040 -c $GRIDDB_CLUSTER_NAMES -n $GRIDDB_NODE_NUM -u $GRIDDB_USERNAME/$GRIDDB_PASSWORD
         fi
         tail -f /var/lib/gridstore/log/gridstore*.log
     fi
